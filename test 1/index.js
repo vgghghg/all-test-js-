98d78865df7d0edbcd9cf46b2c7e844f41e3ecdd
vgghghg <156848481+vgghghg@users.noreply.github.com>
2024-08-09 20:50:09 +0530

    update

--- test 1/index.js
+++ test 1/index.js
@@ -3,18 +3,18 @@
 // Input :[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // output : [1, 3, 5, 7, 9,]
 
- const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//  const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
  
- function removeEven(input){
-    let newInput = [];
-    for (var i = 0; i <= input.lenght -1; i++) {
-        if(input[i] % 2 !== 0){
-            newInput.push(input[i]);
-        }
-    }
-    return newInput;
- }
- console.log(removeEven(input));
+//  function removeEven(input){
+//     let newInput = [];
+//     for (var i = 0; i <= input.lenght -1; i++) {
+//         if(input[i] % 2 !== 0){
+//             newInput.push(input[i]);
+//         }
+//     }
+//     return newInput;
+//  }
+//  console.log(removeEven(input));
 
  //******************************************************************** ***************//
 
@@ -150,25 +150,166 @@
 // console.log(months("April"));
 
 /**************************************************************************************** */
-// 5. write a function that takes an array of numbers and returns anew array with only 
+// 5. write a function that takes an array of numbers and returns a new array with only 
 // the prime number from the original array.
 // Input: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // ouput: [2, 3, 5, 7]
 
-// let input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// function isprime (num) {
-//     if (num === 0 || num === 1) return false;
-//   for  (var i = 2; i < num; i++) {
-//      if (num % i == 0) return false;
-//    }
-//    return true;
-//     
-// for  (var i = 0 i < input.length; i++) {}
+// var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//  var newar =[];
+//  primenum = (arry) => {
+//     for (let i = 0; i < arr.length ; i++){
+//     if (arr[count] / i == 0){
+//    newar.push(arr[i]);
+//  
+//          }
+//     }
+
+// }    
+//
+// console.log(newar);
+
+/*
+
+
+// 6 . write a function that takes three numbers and return the largest of the three
+//     without using the math.max () method.
+//     input : 3, 5, 1
+//     output: 5
+
+    */ 
+
+    // maxnum = (n1,n2,n3) => {
+    //     if (n1 > n2 && n1 > n3 ) console.log(n1);
+    //     else if (n2 > n1 && n1 > n3) console.log(n2);
+    //     else if (n3 > n2 && n1 > n3) console.log(n3);
+
+    // }
+
+    // maxnum(3,5,1);
+    /*
+
+    // 7. write A function that takes an array of number and return the product of all the 
+    //    number in the array .
+    //    Input : [1, 2, 3, 4]
+    //    output: 24
+
+
+  // let input = [1, 2, 3,  4];
+  // function product(input) {
+//         let p = 1;
+//         for (var i = 0; i < input. length i++){
+//         p = p* input[o];
+//       }
+//       return p;
+      
+// }
+// // consol.log (product(input));
+
+/********************************************************************************************* */
+
+// 8. write a function that  takes a string and an optional character. the function should 
+// return the number of times the character appeares in the string . if the character is 
+// not provided cout the number of spaces in the string .
+// input : ("Hello world", "o")
+// Output : 2
+// input : ("Hello world")
+// Output : 1
+
+
+// fuction char count(value , char) {
+//    if (char === ""||) char === undefined) {
+//     let space = 0;
+//      for (var i = 0; i > value .lenght ;i++) {
+//            if (value.charAt === " ") space++;
+//       }
+//       return space;
+//   }
+//   let count = 0;
+//   for (var i = 0; i < value.lenght; i++) {
+//          if (value. charAt(i) === " ") count++;
+//     }
+//    return count;
+// }
+// console.log(charCount("hello world". "o"));
+// console.log(charCount("hello world"));
+
+/******************************************************************** */
+
+
+// 9. write a function that takes a score (0-100) and return the corresponding grade (A, B, C, D, F)
+//  based on standard grading scale . use a switch statement with a default
+//  case for invalid scores.
+// input : 85
+// output : B
+//
+
+// function score(value){
+//     switch (true) {
+//              case value > 85 && value <= 100;
+//                        return "A";
+//                case value > 70 && value <= 85;
+//                       return "B";
+//                  case value > 55 && value <= 70;
+//                        return "C";
+//                  case value > 40 && value <= 55;
+//                         return "D";
+//                  case value > 0 && value <= 40;
+//                         return "F";
+//             default:
+//                     return"Invalid score";
+//
+//   }
+// }
 //
 //
-//        
+//  console.log(score(90));
+//  console.log(score(75));
+//  console.log(score(60));
+//  console.log(score(45));
+//  console.log(score(30));
+//  console.log(score(-5));
+//  console.log(score(105));
+
+/*************************************************************************************** */
+
+// write a function takes an array of strings and return an object where the
+// keys are the string and the values are the lengths of those strings. the function
+//  should exclude strings that have a length or 3 or less.
+// Input : ["apple", "bat", "car", "dolphin"]
+// output: {"apple", 5, "dolphin": 7}
+
+// let Input : ["apple", "bat", "car", "dolphin"]
 
 
+// method-1 (using for loop)
+// function output(input) {
+//      let emptyobj = {};
+//      for (let input of inputs) {
+//      if (input.lenght > 3) {
+//        emptyobj[input] = input.length;
+//     }
+// 
+//   }
+//  return empobj;
+//
+// }
+// console.log(output(inputs));
 
- 
+// method2 (through reduce (input));
+// function output (input) {
+//   let newArray = input.reduce((acc, start)) =>
+//        if (start. lenght > 3) {
+//                   acc[start] = start.lenght;
+// 
+//   }
+//  return acc;
+//   },{});
+//   return newArray;
+// }
+// console.log(output(inputs));
+
+
+/************************************************************************************************************* */
--- test 3/index.js
+++ test 3/index.js
@@ -4,140 +4,140 @@
 // the product's name and price concatenated.
 
 
-const products = [
-     { name: "Laptop", category: "Electronics", price: 1000 },
-    { name: "Shirt", category: "Clothing", price: 50 },
-    { name: "Phone", category: "Electronics", price: 500 },
-    { name: "Shoes", category: "Clothing", price: 80 }
-    ];
+// const products = [
+//      { name: "Laptop", category: "Electronics", price: 1000 },
+//    { name: "Shirt", category: "Clothing", price: 50 },
+//     { name: "Phone", category: "Electronics", price: 500 },
+//     { name: "Shoes", category: "Clothing", price: 80 }
+//     ];
 
 
-function filter(products,category){
+// function filter(products,category){
 
-var flt;
-var mp;
-
-
-flt=products.filter(prod=>{
-   return( prod.category==category);
-});
-console.log(flt);
-
-mp=flt.map(str=>{
-   console.log( `${str.name},${str.price}`)
-
-});
+// var flt;
+// var mp;
 
 
+// flt=products.filter(prod=>{
+//    return( prod.category==category);
+// });
+// console.log(flt);
+
+// mp=flt.map(str=>{
+//    console.log( `${str.name},${str.price}`)
+
+// });
 
 
-}
 
-filter(products,"Electronics");
 
-///////////////////////////////////////////////////////////////////////////////
+// }
+
+// filter(products,"Electronics");
+
+// ///////////////////////////////////////////////////////////////////////////////
     
 
-//  You are given an array of objects representing orders in a restaurant. Each object has three
-// properties: orderId, tableNumber, and amount. Write a function called sumByTable that returns
-// an object where each key is a tableNumber and the value is the total amount for that table.
+// //  You are given an array of objects representing orders in a restaurant. Each object has three
+// // properties: orderId, tableNumber, and amount. Write a function called sumByTable that returns
+// // an object where each key is a tableNumber and the value is the total amount for that table.
 
 
-const orders = [
-	{ orderId: 1, tableNumber: 2, amount: 30 },
-	{ orderId: 2, tableNumber: 1, amount: 20 },
-	{ orderId: 3, tableNumber: 2, amount: 50 },
-	{ orderId: 4, tableNumber: 3, amount: 40 },
-	{ orderId: 5, tableNumber: 1, amount: 10 },
-];
-    function sumByTable(orders) {
-        var ob={};
-        orders.forEach(order => {
+// const orders = [
+// 	{ orderId: 1, tableNumber: 2, amount: 30 },
+// 	{ orderId: 2, tableNumber: 1, amount: 20 },
+// 	{ orderId: 3, tableNumber: 2, amount: 50 },
+// 	{ orderId: 4, tableNumber: 3, amount: 40 },
+// 	{ orderId: 5, tableNumber: 1, amount: 10 },
+// ];
+//     function sumByTable(orders) {
+//         var ob={};
+//         orders.forEach(order => {
 
-            var tno=order.tableNumber;
-            var amt=order.amount;
+//             var tno=order.tableNumber;
+//             var amt=order.amount;
 
 
-            if(ob[tno]){
-                ob[tno]=ob[tno] +amt;}
+//             if(ob[tno]){
+//                 ob[tno]=ob[tno] +amt;}
 
-                else{
-                    ob[tno]=amt;
-                }
+//                 else{
+//                     ob[tno]=amt;
+//                 }
             
-        });
+//         });
 
-        return ob;
-    }
-console.log(sumByTable(orders));
+//         return ob;
+//     }
+// console.log(sumByTable(orders));
 
-// You are given an array of objects representing inventory items. Each object has a name,
-// quantity, and price. Write a function called inventoryValue that returns the total value of the
-// inventory, but only includes items where the quantity is greater than zero.
-
-
-const inventory = [
-{ name: "Laptop", quantity: 5, price: 1000 },
-{ name: "Shirt", quantity: 0, price: 50 },
-{ name: "Phone", quantity: 10, price: 500 },
-{ name: "Shoes", quantity: 3, price: 80 }
-];
+// // You are given an array of objects representing inventory items. Each object has a name,
+// // quantity, and price. Write a function called inventoryValue that returns the total value of the
+// // inventory, but only includes items where the quantity is greater than zero.
 
 
-function new1(inventory){
-    var f=inventory.filter(product=>{
-       return product.quantity>0;
+// const inventory = [
+// { name: "Laptop", quantity: 5, price: 1000 },
+// { name: "Shirt", quantity: 0, price: 50 },
+// { name: "Phone", quantity: 10, price: 500 },
+// { name: "Shoes", quantity: 3, price: 80 }
+// ];
 
-    });
-    // console.log(f);
-    var totalvalue=0;
 
-    var e=f.forEach(ele => {
+// function new1(inventory){
+//     var f=inventory.filter(product=>{
+//        return product.quantity>0;
 
-     totalvalue=totalvalue+(ele.quantity*ele.price);
+//     });
+//     // console.log(f);
+//     var totalvalue=0;
+
+//     var e=f.forEach(ele => {
+
+//      totalvalue=totalvalue+(ele.quantity*ele.price);
         
-    });
+//     });
 
-    return totalvalue;
+//     return totalvalue;
     
  
 
 
 
 
-}
+// }
 
-console.log(new1(inventory));
+// console.log(new1(inventory));
 
-// You are given an array of objects representing transactions. Each object has a userId, amount,
-// and date. Write a function called topSpenders that returns an array of userIds sorted by their
-// total transaction amount in descending order. Only include users who have made more than one
-// transaction.
-const transactions = [
-{ userId: 1, amount: 50, date: "2023-01-01" },
-{ userId: 2, amount: 20, date: "2023-01-02" },
-{ userId: 1, amount: 100, date: "2023-01-03" },
-{ userId: 3, amount: 150, date: "2023-01-04" },
-{ userId: 2, amount: 30, date: "2023-01-05" }
-];
-function topSpenders(transactions) {
-	let uid = {};
-	let realuid = [];
-	for (let trans of transactions) {
-		const { userId } = trans;
-		if (uid[userId]) {
-			uid[userId]++;
-		} else {
-			uid[userId] = 1;
-		}
-	}
-	console.log(uid);
-	let value = Object.keys(uid);
-	// console.log(value);
-	for (let v of value) {
-		if (uid[v] > 1) realuid.push((v));
-	}
-	return realuid;
-	// Your code here
-}
-console.log(topSpenders(transactions));
+// // You are given an array of objects representing transactions. Each object has a userId, amount,
+// // and date. Write a function called topSpenders that returns an array of userIds sorted by their
+// // total transaction amount in descending order. Only include users who have made more than one
+// // transaction.
+// const transactions = [
+// { userId: 1, amount: 50, date: "2023-01-01" },
+// { userId: 2, amount: 20, date: "2023-01-02" },
+// { userId: 1, amount: 100, date: "2023-01-03" },
+// { userId: 3, amount: 150, date: "2023-01-04" },
+// { userId: 2, amount: 30, date: "2023-01-05" }
+// ];
+// function topSpenders(transactions) {
+// 	let uid = {};
+// 	let realuid = [];
+// 	for (let trans of transactions) {
+// 		const { userId } = trans;
+// 		if (uid[userId]) {
+// 			uid[userId]++;
+// 		} else {
+// 			uid[userId] = 1;
+// 		}
+// 	}
+// 	console.log(uid);
+// 	let value = Object.keys(uid);
+// 	// console.log(value);
+// 	for (let v of value) {
+// 		if (uid[v] > 1) realuid.push((v));
+// 	}
+// 	return realuid;
+// 	// Your code here
+// }
+// console.log(topSpenders(transactions));
